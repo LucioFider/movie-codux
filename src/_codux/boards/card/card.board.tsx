2cf6909f0aa38b11301002c8296e7907115f87b4
LucioFider <fidelislucio404@gmail.com>
2023-03-11 10:40:35 +1000

    6th-commit

--- src/_codux/boards/card/card.board.tsx
+++ src/_codux/boards/card/card.board.tsx
@@ -3,5 +3,17 @@ import { Card } from '../../../components/card/card';
 
 export default createBoard({
     name: 'Card',
-    Board: () => <Card />
+    Board: () => (
+        <Card
+            movie={{
+                id: '123',
+                overview:
+                    'Queen Ramonda, Shuri, M’Baku, Okoye and the Dora Milaje fight to protect their nation from intervening world powers in the wake of King T’Challa’s death. As the Wakandans strive to embrace their next chapter, the heroes must band together with the help of War Dog Nakia and Everett Ross and forge a new path for the kingdom of Wakanda.',
+                release_date: '2022',
+                title: 'Wakanda',
+                vote_average: 7,
+                poster_path: 'https://image.tmdb.org/t/p/w500//sv1xJUazXeYqALzczSZ3O6nkH75.jpg',
+            }}
+        />
+    ),
 });
--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -3,12 +3,30 @@ import styles from './card.module.scss';
 
 export interface CardProps {
     className?: string;
+    movie: {
+        id: string;
+        poster_path?: string;
+        title: string;
+        overview: string;
+        vote_average: number;
+        release_date: string;
+    };
 }
 
 /**
  * This component was created using Codux's Default new component template.
  * To create custom component templates, see https://help.codux.com/kb/en/article/configuration-for-cards-and-templates
  */
-export const Card = ({ className }: CardProps) => {
-    return <div className={classNames(styles.root, className)}></div>;
+export const Card = ({ className, movie }: CardProps) => {
+    return (
+        <div className={classNames(styles.root, className)}>
+            <img src={movie.poster_path} className={styles.cardimg} />
+            <div className={styles.detail}>
+                <h1>{movie.title}</h1>
+                <span className={styles.rating}>{movie.vote_average}</span>
+                <p className={styles.desc}>{movie.overview.substring(0,120)}</p>
+                <button className={styles.button}>+</button>
+            </div>
+        </div>
+    );
 };
